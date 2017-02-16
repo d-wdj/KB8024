c542fc39225a4ff23174978c9fb64e5c04f259d9
dmtr13 <dawirjowerdojo@gmail.com>
2017-02-16 12:43:30 +0100

    Project_tree
    Script creates project file tree.

---
+++ make_project_dir.sh
@@ -0,0 +1,21 @@
+# Make a directory with name $1 as the home folder for the project
+echo -n "Please specify the folder name: "
+read foldername
+echo "Creating a folder for project '$foldername'..."
+mkdir $foldername
+cd $foldername
+mkdir Datasets
+mkdir Literature
+mkdir Results
+mkdir ./scripts
+mkdir ./bash
+mkdir ./input
+mkdir ./output
+mkdir ./logs
+touch readme.txt
+echo "Project name: '$foldername'
+Property of Dimitri Wirjowerdojo
+dawirjowerdojo@gmail.com
+Stockholms Universitet - KB8024
+Folder created on $(date +%Y-%m-%d)" > readme.txt
+echo "Project folder '$foldername' has been created."
