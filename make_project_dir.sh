aa7f3311b1a10f4d3446c55868c2a9700f4d0212
dmtr13 <dawirjowerdojo@gmail.com>
2017-02-16 15:48:06 +0100

    Complete timestamp
    Tweaked to include complete time information

--- make_project_dir.sh
+++ make_project_dir.sh
@@ -18,5 +18,7 @@ echo "Project name: '$foldername'
 Property of Dimitri Wirjowerdojo
 $email
 Stockholms Universitet - KB8024
-Folder created on $(date +%Y-%m-%d)" > readme.txt
+MSc Molecular Techniques in Life Science
+KTH, KI, SU & SciLifeLab - 2016/18
+Folder created on $(date)" > readme.txt
 echo "Project folder '$foldername' has been created."
