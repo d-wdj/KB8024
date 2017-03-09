ecfccb480bc0ce058ed49e275883c36fe4ee6293
DMTR <dmtr13@users.noreply.github.com>
2017-03-09 15:28:23 +0100

    Update psi-blast.sh

--- Bash Scripts/psi-blast.sh
+++ Bash Scripts/psi-blast.sh
@@ -1,11 +1,13 @@
-export BLASTDB:=/scratch/blastdb
+export BLASTDB:=/local_uniref/uniref/uniref90
 
 cd Original # Directory of fasta-format files (as input)
 for i in *.fasta
 do
+  if [ ! -f ../Output/$i.psiblast ] ; then 
   echo "Running $i on PSI-BLAST at $(date +%Y-%m-%d:%H:%M:%S)..."
   time psiblast -query $i -evalue 0.01 -db uniref90.fasta -num_iterations 3  -out ../Output/$i.psiblast -out_ascii_pssm ../PSSM/$i.pssm -num_threads 8
   echo "Finished running PSI-BLAST for $i at $(date +%Y-%m-%d:%H:%M:%S).
   "
+  fi
 done
 echo "Job done."
